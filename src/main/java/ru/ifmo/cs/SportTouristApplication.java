2efe855619494ca8276f0196ea42e517718c1401
merfemor <efremov.roman98@gmail.com>
2020-04-18 23:27:05 +0300

    Create project

---
+++ src/main/java/ru/ifmo/cs/SportTouristApplication.java
@@ -0,0 +1,13 @@
+package ru.ifmo.cs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportTouristApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportTouristApplication.class, args);
+	}
+
+}
