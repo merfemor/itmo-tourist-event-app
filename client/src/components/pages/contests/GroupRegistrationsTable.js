315f37d7d82114d75cd229cd13c0fbb43e3ebab1
Roman Efremov <merfemor@yandex-team.ru>
2020-05-03 17:10:31 +0300

    Add group delete button without impl

--- client/src/components/pages/contests/GroupRegistrationsTable.js
+++ client/src/components/pages/contests/GroupRegistrationsTable.js
@@ -1,17 +1,26 @@
 import React from "react";
+import {UserRole} from "../../../api/enums";
+import {If} from "../../../utils/components";
 
 function renderGroupRegistrationCells(group) {
     const membersCount = group.members.length;
     return group.members.map((it, index) =>
         <tr key={`${group.id}/${it.id}`}>
-            {index === 0 && <th rowSpan={membersCount}>{group.id}</th>}
-            {index === 0 && <th rowSpan={membersCount}>{group.name}</th>}
+            <If cond={index === 0}>
+                <th rowSpan={membersCount}>{group.id}</th>
+                <th rowSpan={membersCount}>{group.name}</th>
+            </If>
             <th>
                 {it.lastName + " " + it.firstName + " " + it.middleName}
             </th>
             <th>
                 {it.isMale ? "М" : "Ж"}
             </th>
+            <If cond={index === 0} roleAtLeast={UserRole.VOLUNTEER}>
+                <th rowSpan={membersCount} className="text-right">
+                     <button className="btn btn-danger btn-sm">Удалить</button>
+                </th>
+            </If>
         </tr>
     )
 }
@@ -26,6 +35,9 @@ export function GroupRegistrationsTable(props) {
                 <th>Название группы</th>
                 <th>ФИО</th>
                 <th>Пол</th>
+                <If roleAtLeast={UserRole.VOLUNTEER}>
+                    <th className="text-right pr-2">Действия</th>
+                </If>
             </tr>
             </thead>
             <tbody>
